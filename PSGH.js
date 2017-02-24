95c70867a7f2aeb084f821abb0a8ffce036ef389
Wheatley <ixyled@gmail.com>
2017-02-24 07:44:55 +0000

    emu

--- PSGH.js
+++ PSGH.js
@@ -135,3 +135,38 @@ function secretRupee() {
     secretTune.play();
     console.log('3');
 }
+
+
+
+  var resizeOwnEmulator = function(width, height)
+    {
+        var emulator = $('#emulator');
+        emulator.css('width', width);
+        emulator.css('height', height);
+    }
+
+    $(function()
+    {
+        function embed()
+        {
+            var emulator = $('#emulator');
+            if(emulator)
+            {
+                var flashvars = 
+                {
+                    system : 'sega',
+                    url : '/roms/Flappy Bird (PD) v1.0.gen'
+                };
+                var params = {};
+                var attributes = {};
+
+                params.allowscriptaccess = 'sameDomain';
+                params.allowFullScreen = 'true';
+                params.allowFullScreenInteractive = 'true';
+
+                swfobject.embedSWF('flash/Nesbox.swf', 'emulator', '640', '480', '11.2.0', 'flash/expressInstall.swf', flashvars, params, attributes);
+            }
+        }
+
+        embed();
+    });
